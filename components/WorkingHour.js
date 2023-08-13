8212acdcd9d6cf35bba36b6ec89587ec396361fc
taiwo17 <shoboadefowope2007@gmail.com>
2023-08-13 23:27:41 +0100

    Icon file readjusted

--- components/WorkingHour.js
+++ components/WorkingHour.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import { LocationIcon, MessageIcon, PhoneIcon } from './icons'
+import { LocationIcon, MessageIcon, PhoneIcon } from './Icons'
 
 function WorkingHour() {
   return (
